bd5cf2bd94b60396fc722e40b6f5450de7deb1bb
maxpluez <pipi-max@hotmail.com>
2019-02-17 12:59:41 -0800

    yes

--- Jump/ViewController.swift
+++ Jump/ViewController.swift
@@ -4,22 +4,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var viewDrag: UIView!
     var arrayOfBlocks : Array<UIImageView>;
     var numOfBlocks : Int;
-    var panGesture       = UIPanGestureRecognizer()
     
     required init?(coder aDecoder: NSCoder){
         arrayOfBlocks = []
         numOfBlocks = 0
         super.init(coder: aDecoder);
-    }
-    
-    @objc func draggedView(_ sender:UIPanGestureRecognizer){
-        self.view.bringSubviewToFront(viewDrag)
-        let translation = sender.translation(in: self.view)
-        viewDrag.center = CGPoint(x: viewDrag.center.x + translation.x, y: viewDrag.center.y + translation.y)
-        sender.setTranslation(CGPoint.zero, in: self.view)
     }
     
     
@@ -28,10 +19,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         let block1 : UIButton = self.view.viewWithTag(100) as! UIButton;
         block1.backgroundColor=UIColor.brown;
-        panGesture = UIPanGestureRecognizer(target: self, action: #selector(ViewController.draggedView(_:)))
-        viewDrag.isUserInteractionEnabled = true
-        viewDrag.addGestureRecognizer(panGesture)
-        block1.addGestureRecognizer(panGesture)
+    }
+    
+    @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x:view.center.x + translation.x,
+                                  y:view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
     }
     
     @IBAction func createBlock(){
@@ -40,15 +36,11 @@ class ViewController: UIViewController {
         blockImage.frame = CGRect(x:25, y:25, width:100, height:30)
         blockImage.tag = 1000+numOfBlocks
         blockImage.isUserInteractionEnabled = true;
-        //blockImage.addGestureRecognizer(panGesture)
-        
-        numOfBlocks = numOfBlocks+1
-        //arrayOfBlocks.append(blockImage)
+        blockImage.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:))))
+        blockImage.isUserInteractionEnabled = true;
+        numOfBlocks += 1
+        arrayOfBlocks.append(blockImage)
         self.view.addSubview(blockImage)
     }
-    
-    
-
-
 }
 
