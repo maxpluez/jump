9ec29453f3bf68a160c93132b2d48d154c3aaf5f
maxpluez <pipi-max@hotmail.com>
2019-02-17 11:05:25 -0800

    Add Image by function

--- Jump/ViewController.swift
+++ Jump/ViewController.swift
@@ -1,11 +1,24 @@
 
 import UIKit
 
+extension UIImageView {
+    func setImageColor(color: UIColor) {
+        let templateImage = self.image?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
+        self.image = templateImage
+        self.tintColor = color
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var blockImage : UIImageView = UIImageView();
+        blockImage.backgroundColor=UIColor.brown;
+        blockImage.frame = CGRect(x:50, y:10, width:200, height:100);
+        self.view.addSubview(blockImage);
+        
     }
 
 
